6e412cd4fb09ebe5293a8c8bb0f17da9be5fde17
Palheiros <ricardopalheiros@gmail.com>
2022-03-10 22:07:19 -0300

    First commit

---
+++ ReservationApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using static System.Console;
+using ReservationApp.Entities;
+using ReservationApp.Entities.Exceptions;
+
+namespace ReservationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                WriteLine("Reservation: " + reservation);
+
+                WriteLine();
+                WriteLine("Enter date to update the reservation:");
+                Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                WriteLine("Reservation: " + reservation);
+            }
+            catch (DomainException e)
+            {
+                WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                WriteLine("Format error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
